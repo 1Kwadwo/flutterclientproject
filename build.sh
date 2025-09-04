7b4e1ee9cddfe029fc3d9d771d3aee0724c70876
ESSUA kwadwo-braimah harrison,Mr <harrisbraimah@iCloud.com>
2025-09-04 18:12:31 +0000

    Fix Dart SDK version compatibility for Render deployment

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ set -e
 
 # Install Flutter using the official installation script
 echo "Installing Flutter..."
-curl -O https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.24.5-stable.tar.xz
-tar xf flutter_linux_3.24.5-stable.tar.xz
+curl -O https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.35.3-stable.tar.xz
+tar xf flutter_linux_3.35.3-stable.tar.xz
 export PATH="$PATH:`pwd`/flutter/bin"
 
 # Verify Flutter installation
